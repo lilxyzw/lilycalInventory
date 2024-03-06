145c764d563fe5052337fdec781f2ba5c0a22e51
lilxyzw <lilxyzw@gmail.com>
2024-03-07 02:01:10 +0900

    chore: do not allow duplicate MenuFolders

--- Runtime/MenuFolder.cs
+++ Runtime/MenuFolder.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace jp.lilxyzw.lilycalinventory.runtime
 {
+    [DisallowMultipleComponent]
     [AddComponentMenu(ConstantValues.COMPONENTS_BASE + nameof(MenuFolder))]
     [HelpURL(ConstantValues.URL_DOCS_COMPONENT + nameof(MenuFolder))]
     internal class MenuFolder : MenuBaseComponent
