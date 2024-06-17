1e14149c9e1c948ece77c91aec850bbe75206292
lilxyzw <lilxyzw@gmail.com>
2024-06-17 11:12:34 +0900

    chore: display name

--- Editor/Localization/Localization.cs
+++ Editor/Localization/Localization.cs
@@ -37,15 +37,7 @@ namespace jp.lilxyzw.lilycalinventory
                 var code = Path.GetFileNameWithoutExtension(path);
                 languages.Add(lang);
                 codes.Add(code);
-                try
-                {
-                    var cul = new CultureInfo(code);
-                    tmpNames.Add(cul.NativeName);
-                }
-                catch
-                {
-                    tmpNames.Add(code);
-                }
+                tmpNames.Add(lang.TryGetValue("Language", out string o) ? o : code);
             }
             names = tmpNames.ToArray();
             number = GetIndexByCode(LoadLanguageSettings());
