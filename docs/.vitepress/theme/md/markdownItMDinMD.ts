40f48944766328482b1fd2305dcd9cb3aed2fe30
lilxyzw <lilxyzw@gmail.com>
2024-08-12 00:15:18 +0900

    chore: output the error as is

--- docs/.vitepress/theme/md/markdownItMDinMD.ts
+++ docs/.vitepress/theme/md/markdownItMDinMD.ts
@@ -8,13 +8,8 @@ const markdownItMDinMD: PluginSimple = (md) => {
   const parse = (src) => {
     var cap;
     while(cap = reg.exec(src)) {
-      let path = cap[1].trim();
-      var txt;
-      if(fs.existsSync(path)) {
-        txt = parse(fs.readFileSync(path, 'utf8'));
-      } else {
-        txt = `\'${path}\' not found.`;
-      }
+      var path = cap[1].trim();
+      var txt = parse(fs.readFileSync(path, 'utf8'));
       src = src.substring(0, cap.index) + txt + src.substring(cap.index + cap[0].length);
     }
     return src;
