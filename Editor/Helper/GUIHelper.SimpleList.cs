6a61802a846e6964b54e304746cfa0dd445cb290
fiko <nspyia2002@gmail.com>
2025-02-27 13:35:34 +0800

    feat: make original materials clickable in Material Replacer (#151)
    * fix: version check issues #148
    
    * Fix version check issues
    
    Fix editor unable to display due to version check network issues
    
    * Update VersionChecker.cs
    
    Ensure that instance.latest is not null
    
    * feat: make original materials clickable in Material Replacer
    
    * fix: `AutoDresser` preview parameter application
    
    ---------
    
    Co-authored-by: 宵夜 <34893504+xiaoye97@users.noreply.github.com>

--- Editor/Helper/GUIHelper.SimpleList.cs
+++ Editor/Helper/GUIHelper.SimpleList.cs
@@ -6,15 +6,28 @@ namespace jp.lilxyzw.lilycalinventory
     internal static partial class GUIHelper
     {
         // リストのかわりにプロパティを並べて表示するだけのシンプルなもの
-        internal static Rect SimpleList(SerializedProperty prop, Rect position, string[] labels)
+        internal static Rect SimpleList(SerializedProperty prop, Rect position, Material[] originalMaterials)
         {
             using var endProperty = prop.GetEndProperty();
             prop.NextVisible(true);
             position.Back();
             int i = 0;
-            while(prop.NextVisible(false) && !SerializedProperty.EqualContents(prop, endProperty))
+            while (prop.NextVisible(false) && !SerializedProperty.EqualContents(prop, endProperty))
             {
-                EditorGUI.PropertyField(position.NewLine(), prop, new GUIContent(labels[i++]));
+                var rect = position.NewLine();
+
+                var materialRect = rect;
+                materialRect.width = rect.width * 0.5f - 2;
+                EditorGUI.BeginDisabledGroup(true);
+                EditorGUI.ObjectField(materialRect, originalMaterials[i], typeof(Material), false);
+                EditorGUI.EndDisabledGroup();
+
+                var replaceRect = rect;
+                replaceRect.x += materialRect.width + 4;
+                replaceRect.width = rect.width * 0.5f - 2;
+                EditorGUI.PropertyField(replaceRect, prop, GUIContent.none);
+
+                i++;
             }
             return position;
         }
